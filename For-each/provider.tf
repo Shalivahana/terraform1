7b71e9c5809f99b96f95dd071cfc006f67a06c2f
Chary.manu <chary.manu78@gmail.com>
2024-09-29 14:40:49 +0530

    terraform-remote

---
+++ For-each/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.68.0"
+    }
+  }
+
+backend  "s3" {
+    bucket = "ss-remote-state"
+    key = "for-each-demo"
+    region = "us-east-1"
+    dynamodb_table = "ss-locking"
+    }
+  
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
